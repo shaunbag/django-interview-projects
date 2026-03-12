ac24d9862046367fdd7911088d13481e6aa6443f
Shaun <shaun@vegascity,org>
2026-03-12 17:18:23 +0000

    Decorators and url corrections

--- interview_projects/todo/urls.py
+++ interview_projects/todo/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
-    path("toggle_complete/<int:todo_id>/", views.toggle_complete, name="toggle_complete"),
+    path('index_todo', views.IndexView.as_view(), name='index_todo'),
+    path('toggle_complete/<int:todo_id>/', views.toggle_complete, name='toggle_complete'),
     path('delete_todo/<int:todo_id>', views.delete_todo, name='delete_todo'),
     path('add_todo/', views.add_todo, name='add_todo'),
     path('add_todo_page/', views.add_todo_page, name='add_todo_page'),
--- interview_projects/todo/views.py
+++ interview_projects/todo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, get_object_or_404, render
 from django.views.generic import ListView
 from .models import TodoList, Todo
 from .forms import TodoForm
+from django.views.decorators.http import require_http_methods
 
 
 class IndexView(ListView):
@@ -10,29 +11,30 @@ class IndexView(ListView):
     context_object_name = "todo_lists"
 
 
+@require_http_methods(['POST'])
 def toggle_complete(request, todo_id):
-    if request.method == 'POST':
-        todo = get_object_or_404(Todo, id=todo_id)
-        todo.is_completed = not todo.is_completed
-        todo.save()
-        return redirect('index')
-    
+    todo = get_object_or_404(Todo, id=todo_id)
+    todo.is_completed = not todo.is_completed
+    todo.save()
+    return redirect('index_todo')
 
+
+@require_http_methods(['POST'])
 def delete_todo(request, todo_id):
-    if request.method == 'POST':
-        todo = get_object_or_404(Todo, id=todo_id)
-        todo.delete()
-        return redirect('index')
-    
+    todo = get_object_or_404(Todo, id=todo_id)
+    todo.delete()
+    return redirect('index_todo')
 
+
+@require_http_methods(['POST'])
 def add_todo(request):
-    if request.method == 'POST':
-        form = TodoForm(request.POST)
-        if form.is_valid():
-           form.save()
-        return redirect('index')
+    form = TodoForm(request.POST)
+    if form.is_valid():
+        form.save()
+    return redirect('index_todo')
 
 
+@require_http_methods(['GET'])
 def add_todo_page(request):
     if request.method == 'GET':
         form = TodoForm()
